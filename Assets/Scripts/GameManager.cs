a844db77368c57ca3da5e35eff04281b87192d0c
Facundo <faquidg@hotmail.com>
2026-02-24 20:30:53 +0100

    chore: update GameManager.cs file

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,7 +9,6 @@ public class GameManager : MonoBehaviour
         if (Instance == null)
         {
             Instance = this;
-            DontDestroyOnLoad(gameObject);
         }
         else
         {
