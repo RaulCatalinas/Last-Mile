87a0b468cafb55a7b60d01212723a291a609a7b1
Raul Catalinas <raulcatalinas@gmail.com>
2026-02-26 00:38:12 +0100

    chore: update ObstacleController.cs file

--- Assets/Scripts/ObstacleController.cs
+++ Assets/Scripts/ObstacleController.cs
@@ -3,17 +3,18 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class ObstacleController : MonoBehaviour
 {
-
     private Rigidbody2D rb;
+    private float speed;
 
     void Awake()
     {
+        speed = ObstacleSpawner.Instance.GetRandomSpeed();
         rb = GetComponent<Rigidbody2D>();
     }
 
     void FixedUpdate()
     {
-        rb.linearVelocityX = -ObstacleSpawner.Instance.GetRandomSpeed();
+        rb.linearVelocityX = -speed;
     }
 
     void OnTriggerEnter2D(Collider2D collision)
