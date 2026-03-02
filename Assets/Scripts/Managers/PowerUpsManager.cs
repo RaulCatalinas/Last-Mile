49e351a1d0690da98f42d955ed81cae9368ec2a3
Facundo <faquidg@hotmail.com>
2026-03-02 20:55:11 +0100

    chore: add "PowerUpsManager" file for future power-up logic

---
+++ Assets/Scripts/Managers/PowerUpsManager.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpsManager : MonoBehaviour
+{
+    [SerializeField] private List<PowerUpData> powerUps;
+    [SerializeField] private List<PowerUpData> trollPowerUps;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
